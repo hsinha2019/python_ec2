89a1d9bec24be114d2e816fcc6845bc068b1eede
Harshita Srivastava <harshitasrivastava@Harshitas-MacBook-Air.local>
2019-05-26 12:39:50 -0400

    setup.py

--- ec2/listEC2.py
+++ ec2/listEC2.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+import botocore
 
 session = boto3.Session(profile_name='hawli')
 ec2 = session.resource('ec2')
@@ -15,9 +16,62 @@ def filterInstances(project):
 	return instances	
 
 @click.group()
+def cli():
+	"""Common group for all functions"""
+
+@cli.group('volumes')
+def volumes():
+	"""Commands for volumes"""
+	
+@cli.group('snapshots')
+def snapshots():
+	"""Commands for snapshots"""	
+
+@cli.group('instances')
 def instances():
 	"""Commands for instances"""
-
+	
+@snapshots.command('list')
+@click.option('--project',default=None,help="Only instances for Project (tag Project:<name>)")
+def listInstances(project):
+	"List EC2 Snapshots"
+	
+	instances = filterInstances(project)		
+	for e in instances:
+		for v in e.volumes.all():
+			for s in v.snapshots.all():
+				print(" , ".join((e.id,
+				e.placement['AvailabilityZone'],
+				e.state['Name'],v.volume_id,v.state)))
+				
+@snapshots.command('create')
+@click.option('--project',default=None,help="Only instances for Project (tag Project:<name>)")
+def listInstances(project):
+	"Create EC2 Snapshots"
+	
+	instances = filterInstances(project)		
+	for e in instances:
+		e.stop()
+		e.wait_until_stopped()
+		for v in e.volumes.all():
+			print("Creating snapshot of {0}".format(v.id))
+			v.create_snapshot(Description="Created by hawli")
+		e.start()
+		e.wait_until_running()
+	return				
+	
+@volumes.command('list')
+@click.option('--project',default=None,help="Only instances for Project (tag Project:<name>)")
+def listInstances(project):
+	"List EC2 Volumes"
+	
+	instances = filterInstances(project)		
+	for e in instances:
+		for v in e.volumes.all():
+			print(" , ".join((e.id,
+			e.placement['AvailabilityZone'],
+			e.state['Name'],v.volume_id,v.state)))
+  
 @instances.command('list')
 @click.option('--project',default=None,help="Only instances for Project (tag Project:<name>)")
 def listInstances(project):
@@ -37,8 +91,12 @@ def stopInstances(project):
 	"Stop EC2 Instances"
 	instances = filterInstances(project)
 	for e in instances:
-		print("Stopping {0} ..".format(e.id))
-		e.stop()
+		try:
+			print("Stopping {0} ..".format(e.id))
+			e.stop()
+		except botocore.exceptions.ClientError as Ex:
+			print("Could not stop {0}".format(e.id) + str(e))
+			continue	
 	return	
 	 	 
 @instances.command('start')
@@ -47,11 +105,15 @@ def stopInstances(project):
 	"Start EC2 Instances"
 	instances = filterInstances(project)
 	for e in instances:
-		print("Starting {0} ..".format(e.id))
-		e.start()
+		try:
+			print("Starting {0} ..".format(e.id))
+			e.start()
+		except botocore.exceptions.ClientError as Ex:
+			print("Could not start {0}".format(e.id) + str(e))
+			continue
 	return	
 
 if __name__ == '__main__':
 	
-	instances()
+	cli()
 
